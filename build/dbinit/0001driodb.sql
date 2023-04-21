35dd6f36609432e845caab66e77e9309d0a7dbb9
sraghavan-drio <sraghavan@drio.ai>
2023-04-21 15:41:00 -0700

    Fixed syntax error. Create trigger after creating table

--- build/dbinit/0001driodb.sql
+++ build/dbinit/0001driodb.sql
@@ -14,21 +14,21 @@ create or replace function accounts.trigger_update_timestamp() returns trigger a
     end;
 $update_ts$ language plpgsql;
 
-create trigger update_timestamp
-before update on accounts.accounts
-for each row
-execute procedure trigger_update_timestamp();
-
 create table if not exists accounts.accounts (
     id               uuid default gen_random_uuid() primary key,
     name             varchar(:maxnamelen) not null check (length(name) >= 1),
     created_at       timestamptz not null default now(),
-    updated_at       timestamptz not null default now()
+    updated_at       timestamptz not null default now(),
     country          varchar(:countrycodelen) not null check (length(country) = :countrycodelen),
     state            varchar(:maxnamelen) not null check (length(name) >= 1),
     city             varchar(:maxnamelen) not null check (length(name) >= 1),
     details          jsonb
 );
+
+create trigger update_timestamp
+before update on accounts.accounts
+for each row
+execute procedure accounts.trigger_update_timestamp();
 
 create table if not exists accounts.ou (
     id int primary key,
