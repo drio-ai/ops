2fd98b572d868603c434b2bc92d992209d8c148f
sraghavan-drio <sraghavan@drio.ai>
2023-04-20 21:53:12 -0700

    Setup id using built in. Setup created_at and updated_at fields using built in.

--- build/dbinit/0001driodb.sql
+++ build/dbinit/0001driodb.sql
@@ -4,9 +4,26 @@ create database drio;
 
 create schema accounts;
 
-create table if not exists accounts.user (
-    id int primary key,
-    name varchar
+\set maxnamelen 256
+\set idlen      64
+
+create or replace function accounts.trigger_update_timestamp() returns trigger as $update_ts$
+    begin
+        NEW.updated_at = now();
+        return NEW;
+    end;
+$update_ts$ language plpgsql;
+
+create trigger update_timestamp
+before update on accounts.accounts
+for each row
+execute procedure trigger_update_timestamp();
+
+create table if not exists accounts.accounts (
+    id               uuid default gen_random_uuid() primary key,
+    name             varchar(:maxnamelen) not null check (length(name) >= 1),
+    created_at       timestamptz not null default now(),
+    updated_at       timestamptz not null default now()       
 );
 
 create table if not exists accounts.ou (
