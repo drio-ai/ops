3532870b710f53a78b7c8a3a368e41d2c2ec8cad
sraghavan-drio <sraghavan@drio.ai>
2023-04-21 16:07:00 -0700

    Fixed and name and city constraints. Added ou and user tables

--- build/dbinit/0001driodb.sql
+++ build/dbinit/0001driodb.sql
@@ -6,6 +6,7 @@ create schema accounts;
 
 \set maxnamelen 256
 \set countrycodelen 2
+\set maxemaillen 1024
 
 create or replace function accounts.trigger_update_timestamp() returns trigger as $update_ts$
     begin
@@ -20,8 +21,8 @@ create table if not exists accounts.accounts (
     created_at       timestamptz not null default now(),
     updated_at       timestamptz not null default now(),
     country          varchar(:countrycodelen) not null check (length(country) = :countrycodelen),
-    state            varchar(:maxnamelen) not null check (length(name) >= 1),
-    city             varchar(:maxnamelen) not null check (length(name) >= 1),
+    state            varchar(:maxnamelen) not null check (length(state) >= 1),
+    city             varchar(:maxnamelen) not null check (length(city) >= 1),
     details          jsonb
 );
 
@@ -30,10 +31,49 @@ before update on accounts.accounts
 for each row
 execute procedure accounts.trigger_update_timestamp();
 
+/*
+ * We will define no action for foreign key constraint. We are starting off with
+ * not allowing delete in accounts table as long as there are references from ou.
+ */
 create table if not exists accounts.ou (
-    id int primary key,
-    name varchar
+    id               uuid default gen_random_uuid() primary key,
+    name             varchar(:maxnamelen) not null check (length(name) >= 1),
+    created_at       timestamptz not null default now(),
+    updated_at       timestamptz not null default now(),
+    account_id       uuid not null,
+    country          varchar(:countrycodelen) not null check (length(country) = :countrycodelen),
+    state            varchar(:maxnamelen) not null check (length(state) >= 1),
+    city             varchar(:maxnamelen) not null check (length(city) >= 1),
+    details          jsonb,
+    constraint fk_accounts
+        foreign key(account_id)
+            references accounts.accounts(id)
 );
+
+create trigger update_timestamp
+before update on accounts.ou
+for each row
+execute procedure accounts.trigger_update_timestamp();
+
+create table if not exists accounts.user (
+    id               uuid default gen_random_uuid() primary key,
+    first_name       varchar(:maxnamelen) not null check (length(first_name) >= 1),
+    last_name        varchar(:maxnamelen) not null check (length(last_name) >= 1),
+    created_at       timestamptz not null default now(),
+    updated_at       timestamptz not null default now(),
+    email            varchar(:maxemaillen) not null check (length(email) >= 1),
+    country          varchar(:countrycodelen) not null check (length(country) = :countrycodelen),
+    state            varchar(:maxnamelen) not null check (length(state) >= 1),
+    city             varchar(:maxnamelen) not null check (length(city) >= 1),
+    active           boolean not null,
+    scope            jsonb,
+    details          jsonb
+);
+
+create trigger update_timestamp
+before update on accounts.user
+for each row
+execute procedure accounts.trigger_update_timestamp();
 
 create schema config;
 
