0568ed06a64c2730722133af6f5dfcfe509a66f5
sraghavan-drio <sraghavan@drio.ai>
2023-05-09 10:34:23 -0700

    Removed ctrlops script. Replaced hardcoding with arguments. Initialize redis with password. TODO: Persist this change

--- controller/docker/dbinit/0001setpassword.sh
+++ controller/docker/dbinit/0001setpassword.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+if [ -z ${VAULT_ADDR} ]; then
+    echo "Did not find VAULT_ADDR in environment"
+    exit 1
+fi
+
 if [ ! -z ${VAULT_ROLE_ID} ] && [ ! -z ${VAULT_SECRET_ID} ]; then
     echo "Vault login attempt..."
-    vault_client_token_resp=$(curl --request POST --data "{\"role_id\": \"${VAULT_ROLE_ID}\", \"secret_id\": \"${VAULT_SECRET_ID}\"}" http://vault:8200/v1/auth/approle/login)
+    vault_client_token_resp=$(curl --request POST --data "{\"role_id\": \"${VAULT_ROLE_ID}\", \"secret_id\": \"${VAULT_SECRET_ID}\"}" ${VAULT_ADDR}/v1/auth/approle/login)
     vault_client_token=$(echo ${vault_client_token_resp} | jq -r ".auth.client_token")
 
     if [ -z ${vault_client_token} ]; then
@@ -11,7 +16,7 @@ if [ ! -z ${VAULT_ROLE_ID} ] && [ ! -z ${VAULT_SECRET_ID} ]; then
     fi
 
     echo "Vault login was successful. Fetching DB password from vault..."
-    vault_db_password_resp=$(curl --header "X-Vault-Token: ${vault_client_token}" http://vault:8200/v1/drio-controller/ops/data/postgres)
+    vault_db_password_resp=$(curl --header "X-Vault-Token: ${vault_client_token}" ${VAULT_ADDR}/v1/drio-controller/ops/data/postgres)
     vault_db_password=$(echo ${vault_db_password_resp} | jq -r ".data.data.password")
 
     if [ -z ${vault_db_password} ]; then
