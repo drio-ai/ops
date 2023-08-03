5abe331867561579fa80205b994d512376be6535
sraghavan-drio <sraghavan@drio.ai>
2023-08-03 14:14:48 -0700

    Added a script to get SaaS admin password from vault

---
+++ vault/scripts/get-saas-admin-pass.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script should never be copied into the vault docker image
+
+docker exec -ti vault /bin/sh -c "VAULT_TOKEN=\$(jq -r .root_token /vault/tokens/root/secrets) vault kv get -field password -mount drio-controller/ops saas-admin@drio.ai"
