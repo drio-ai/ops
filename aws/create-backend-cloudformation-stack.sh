a73c882062e5f4e066da9a2b4a377c966f8ba909
Sushil Sanjay Bhile <sushilmaxbhile@gmail.com>
2024-07-01 12:27:36 +0530

    Updating scripts to use variable values from single source file

--- aws/create-backend-cloudformation-stack.sh
+++ aws/create-backend-cloudformation-stack.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Variables
-VPC_NAME="Backend-VPC"
-STACK_NAME="BackendStack"
-REGION="us-west-2"
-TEMPLATE_FILE="backend-vpc.yaml"
+# Setting environment variables using single source file
+source ./aws.env
 
-# Function to check if VPC with name $VPC_NAME already exists
+# Function to check if VPC with name $BACKEND_VPC_NAME already exists
 check_VPC_exists() {
     # Check if VPC with the given name already exists
-    VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=${VPC_NAME}" --query "Vpcs[0].VpcId" --output text --region ${REGION})
+    VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=${BACKEND_VPC_NAME}" --query "Vpcs[0].VpcId" --output text --region ${BACKEND_REGION})
     if [[ $VPC_ID == "None" ]]; then
-        echo "VPC with name ${VPC_NAME} does not exist"
+        echo "VPC with name ${BACKEND_VPC_NAME} does not exist"
         return 1
     else
-        echo "VPC with name ${VPC_NAME} already exists with VPC ID: ${VPC_ID}. Skipping stack creation."
+        echo "VPC with name ${BACKEND_VPC_NAME} already exists with VPC ID: ${VPC_ID}. Skipping stack creation."
         return 0
     fi
 }
@@ -22,13 +19,13 @@ check_VPC_exists() {
 # Function to create the CloudFormation stack
 create_stack() {
     echo "Creating CloudFormation stack..."
-    aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://${TEMPLATE_FILE} --capabilities CAPABILITY_NAMED_IAM --region ${REGION}
+    aws cloudformation create-stack --stack-name ${BACKEND_STACK_NAME} --template-body file://${BACKEND_VPC_TEMPLATE_FILE} --capabilities CAPABILITY_NAMED_IAM --region ${BACKEND_REGION}
 }
 
 # Function to wait for the stack to complete
 wait_for_stack() {
     echo "Waiting for CloudFormation stack to complete..."
-    aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region $REGION
+    aws cloudformation wait stack-create-complete --stack-name $BACKEND_STACK_NAME --region $BACKEND_REGION
     if [ $? -eq 0 ]; then
         echo "Stack creation completed successfully."
     else
@@ -40,7 +37,7 @@ wait_for_stack() {
 # Function to get stack output in JSON format
 get_stack_output() {
     echo "Fetching stack output..."
-    OUTPUT=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query "Stacks[0].Outputs" --output json)
+    OUTPUT=$(aws cloudformation describe-stacks --stack-name $BACKEND_STACK_NAME --region $BACKEND_REGION --query "Stacks[0].Outputs" --output json)
 
     echo "Stack output:"
     if command -v jq &> /dev/null; then
